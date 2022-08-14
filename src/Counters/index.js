a51ffab8d3d0f37468766d9953d231f552e71bdf
Your Name <you@example.com>
2022-08-13 21:31:04 -0300

    Conectado front y back

--- src/Counters/index.js
+++ src/Counters/index.js
@@ -6,86 +6,67 @@ import '../App.css'
 import { fetchBack } from "../helpers/fetchBack";
 
 const Index = () => {
-  const { reqCharacter, letrasCharacter } = FindCharacter();
-  const { reqLocation, letrasLocation } = FindLocation();
-  const { reqEpisode, letrasEpisode } = FindEpisode();
-  const { requestFetch } = fetchBack();
 
-  const [countCharacter, setCountCharacter] = useState();
-  const [countLocation, setCountLocation] = useState();
-  const [countEpisode, setCountEpisode] = useState();
-  const [show, setShow] = useState();
+  // const { reqCharacter, letrasCharacter } = FindCharacter();
+  // const { reqLocation, letrasLocation } = FindLocation();
+  // const { reqEpisode, letrasEpisode } = FindEpisode();
+  // const { requestFetch } = fetchBack();
 
-  const clickCharacter = () => {
-    setCountCharacter( letrasCharacter );
-  };
+  // const [countCharacter, setCountCharacter] = useState();
+  // const [countLocation, setCountLocation] = useState();
+  // const [countEpisode, setCountEpisode] = useState();
+  
 
-  const clickLocation = () => {
-    setCountLocation( letrasLocation );
-  };
+  // const clickCharacter = () => {
+  //   setCountCharacter( letrasCharacter );
+  // };
 
-  const clickEpisode = () => {
-    setCountEpisode( letrasEpisode );
-  };
+  // const clickLocation = () => {
+  //   setCountLocation( letrasLocation );
+  // };
 
-  const clickShow = () => {
-    setShow( requestFetch )
+  // const clickEpisode = () => {
+  //   setCountEpisode( letrasEpisode );
+  // };
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    console.log('hola')
+    
+   
   }
+
+  
 
   return (
     <>
-        <div className="App" > 
+        {/* <div className="App" > 
         <h1 className="title" > Desafio Rick And Morty </h1>     
         <div className="order">
      <button  className="btn-search" onClick={ reqCharacter }> character </button>
-      <button  className="btn-search" onClick={ clickCharacter }> letrascharacter </button>
+      <button  className="btn-search" onClick={ clickCharacter }> letrascharacter </button> */}
 
-      <button onClick={ reqLocation }> location </button>
-      <button onClick={ clickLocation }> letras location </button>
+     
+     <a onClick={() => handleClick() } target="_blank" href='http://localhost:3001/'  >
+      Click me
+    
 
-      <button onClick={ reqEpisode }> Episode </button>
-      <button onClick={ clickEpisode }> letras Episode </button>
-      <button onClick={ clickShow }> show </button>
+     </a>
+
+     
+     
 
         
       <div> 
 
       <div>
-        { show }
+        
       </div>
       
-      {
-        countCharacter ? JSON.stringify({ 
-        char: 'c',
-        count: countCharacter,
-        resource: 'character'
-        }) : null } 
-        
-        </div>
-     
-      <div> 
-      { 
-        countLocation ? JSON.stringify({
-            char: 'l',
-            count: countLocation,
-            resource: 'location'
-        }) : null
-        
-        } </div>
-     
-     
-      <div> 
-      { 
-        countEpisode ? JSON.stringify({
-            char: 'e',
-            count: countEpisode,
-            resource: 'episode'
-        }) : null
-        
-        } </div>
+     </div>
 
-      </div>
-      </div>
+      {/* </div>
+      </div> */}
 
     </>
   );
